8ae22e0c2859b0502a080da3ee941d95ef830c09
Małgorzata Skrudlik <sophie.gosia@gmail.com>
2018-04-09 19:51:25 +0200

    Fix js indentation

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 // changes in CSS - jquery
-var span = $('span');
-span.each(function(index, element) {
-	$('span:even').css('color', 'red');
+var $span = $('span');
+$span.each(function(index, element) {
+  $('span:even').css('color', 'red');
 });
 
 // Color change - 2 possible ways
-/*span.each(function(index, element) {
+/*$span.each(function(index, element) {
 	if(index % 2 == 0) {
 		$(element).css('color', 'red');
 	};
 }); */
 
 // Add new elements in jquery
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-    $(element).append(button);
+var $paragraphs = $('p');
+$paragraphs.each(function(index, element) {
+  var $button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+  $(element).append(button);
 });
 
 // Event handlers
